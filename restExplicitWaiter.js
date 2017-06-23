e527f492da654e2679e93b974927fc55406f7ae2
Gennadii <g.mishchevskii@gmail.com>
2017-06-23 13:33:24 +0300

    removed browser, added waitForStatement

--- restExplicitWaiter.js
+++ restExplicitWaiter.js
@@ -1,3 +1,14 @@
+function waitForStatement = function (cb, timeOut, interval) {
+    log.info(`Waiting for statement ${cb.toString()}...`);
+    timeOut = timeOut || 3 * 60 * 1000;
+    interval = interval || 100;
+    let currentTime = () => +(new Date()),
+        startTime = currentTime(),
+        continuePolling = () => (currentTime() - startTime) < timeOut,
+        callback = () => Promise.resolve().then(cb);
+    return _pollRest(callback, continuePolling, interval, false);
+};
+
 function waitForRest(callback, timeOut, interval) {
     timeOut = timeOut || 60 * 1000;
     interval = interval || 2 * 1000;
@@ -17,7 +28,12 @@ function retryRest(callback, retryNumber, interval) {
 function _pollRest(callback, continuePolling, interval, cbContinueCondition) {
     return !continuePolling() ? Promise.reject(`Polling didn't give results.`) :
         callback()
-            .then(response => response !== cbContinueCondition ? response :
-                browser.sleep(interval)
+            .then(response => response !== cbContinueCondition ? response : 
+                  _sleep(interval)
                     .then(() => _pollRest(callback, continuePolling, interval, cbContinueCondition)))
 }
+
+function _sleep = function (ms) {
+    if (typeof ms !== 'number') throw new Error('No argument was given to delayActivity');
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
